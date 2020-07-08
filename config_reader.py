9fd09df18d7dc67b015cfcf91379bc846aa8f55f
Subramonia Siva <subramonia.siva@gmail.com>
2020-07-07 23:09:06 -0700

    Changed the template. removed the template path from cfg.ini. defaulted the template path in config reader

--- config_reader.py
+++ config_reader.py
@@ -6,14 +6,15 @@ config = configparser.ConfigParser()
 #config.read(r'cfg.ini')
 print(os.path.dirname(os.path.realpath(__file__)))
 ini_file = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'cfg.ini'
+default_template_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'templates' + os.path.sep
 config.read(ini_file)
 
 def get_config(env='PROD'):
-    defaults_dic = {'year':datetime.today().strftime('%Y'),'per':'01'}
+    defaults_dic = {'year':datetime.today().strftime('%Y'),'per':'01','template_path':default_template_path}
     if env is None :
         env = 'PROD'
     input_path = config.get(env, 'input_path')
-    template_path = config.get(env, 'template_path')
+    template_path = config.get(env, 'template_path',fallback=defaults_dic['template_path'])
     output_path = config.get(env, 'output_path')
     myyear = config.get(env,'year',fallback=defaults_dic['year'])
     myper = config.get(env,'per',fallback=defaults_dic['per'])
@@ -28,5 +29,5 @@ def get_config(env='PROD'):
     else:
         print("Environment parameter is not matching")
 
-    print("input path = " + input_path)
+    print(input_path,template_path,output_path,myyear,myper)
     return (input_path,template_path,output_path,myyear,myper)
