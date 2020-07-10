35234ce0beb3446c8aa6edd8adc2bf314528edca
Subramonia Siva <subramonia.siva@gmail.com>
2020-07-10 04:24:44 -0700

    config reader changes regarding PL and TB folder

--- tb_multiple_bus_lc.py
+++ tb_multiple_bus_lc.py
@@ -1,17 +1,17 @@
 from merge_sheets_by_columns import  *
 from config_reader import *
 # Prepare the spreadsheets to copy from and paste too.
-(input_path,template_path,output_path,myyear,myper) = get_config(env=sys.argv[1] if len(sys.argv) > 1 else None)
+(TB_input_path,PL_input_path,template_path,TB_output_path,PL_output_path,myyear,myper) = get_config(env=sys.argv[1] if len(sys.argv) > 1 else None)
 # File to be copied
 workbook_base = 'A510_TB_Multiple BUs_LC_All Bus'
-workbook = fnmatch.filter(os.listdir(input_path), '*A510_TB_Multiple BUs_LC_All Bus*')
+workbook = fnmatch.filter(os.listdir(TB_input_path), '*A510_TB_Multiple BUs_LC_All Bus*')
 workbook1 = workbook[0]
 print("File Names are " + workbook[0])
-workbook_url = r'{}{}'.format(input_path,workbook[0])
+workbook_url = r'{}{}'.format(TB_input_path,workbook[0])
 print("Workbook URLs are " + workbook_url)
 workbook_template = r'{}{}.xltx' .format(template_path,workbook_base)
 print("Template URL is " + workbook_template)
-result_workbook = r'{}{}_combined.xlsx'.format(output_path,workbook1.rsplit('.',1)[0])
+result_workbook = r'{}{}_combined.xlsx'.format(TB_output_path,workbook1.rsplit('.',1)[0])
 print("Result workbook URLs are " + result_workbook)
 
 wb = openpyxl.load_workbook(workbook_url)  # Add file name
