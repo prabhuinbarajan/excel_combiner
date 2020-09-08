0e2e79f8d7deccfa6530e419363c5d35d3a99fe9
ssiva02 <subramonia.siva@gmail.com>
2020-09-08 04:43:39 -0700

    Changes for PL61_Dept Cost By GAAP Line, FTE HC, EMP HC and CNTR HC sheet merges for exec summary table final report.

--- exe_summary_table.py
+++ exe_summary_table.py
@@ -1,7 +1,7 @@
 from merge_sheet_by_rows import *
 from config_reader import *
 import xlwings as xw
-import pandas as pd
+# import pandas as pd
 # Prepare the spreadsheets to copy from and paste too.
 (TB_input_path,PL_input_path,template_path,TB_output_path,PL_output_path,myyear,myper) = get_config(env=sys.argv[1] if len(sys.argv) > 1 else None)
 
@@ -13,15 +13,33 @@ workbook_t19 = fnmatch.filter(os.listdir(PL_input_path), '*PL59_Dept Cost By Cat
 workbook_t18 = fnmatch.filter(os.listdir(PL_input_path), '*PL62_Dept Cost by Mgt Group*')
 workbook_t17 = fnmatch.filter(os.listdir(PL_input_path), '*PL67_GAAP GA Line_BES*')
 workbook_t20 = fnmatch.filter(os.listdir(PL_input_path), '*PL60_Dept Cost by Category by Seg*')
+workbook_t21 = fnmatch.filter(os.listdir(PL_input_path), '*PL61_Dept Cost By GAAP Line*')
+workbook_t22 = fnmatch.filter(os.listdir(PL_input_path), '*PL65_FTE ACT VS FCST*')
+workbook_t23 = fnmatch.filter(os.listdir(PL_input_path), '*PL66_FTE CY vs PY*')
+workbook_t24 = fnmatch.filter(os.listdir(PL_input_path), '*PL63_EMP HC ACT VS FCST*')
+workbook_t25 = fnmatch.filter(os.listdir(PL_input_path), '*PL64_EMP HC CY vs PY*')
+workbook_t26 = fnmatch.filter(os.listdir(PL_input_path), '*PL57_CNTR HC Act vs FCST*')
+workbook_t27 = fnmatch.filter(os.listdir(PL_input_path), '*PL58_CNTR HC CY vs PY*')
 
-print("File Names are " + workbook_t19[0] + ", " + workbook_t18[0] + ", " + workbook_t17[0] + ", " + workbook_t20[0])
+print("File Names are " + workbook_t19[0] + ", " + workbook_t18[0] + ", " + workbook_t17[0] + ", " + workbook_t20[0]
+      + ", " + workbook_t21[0] + ", " + workbook_t22[0] + ", " + workbook_t23[0] + ", " + workbook_t24[0]
+      + ", " + workbook_t25[0] + ", " + workbook_t26[0] + ", " + workbook_t27[0])
 
 workbook_url = r'{}{}'.format(PL_input_path,workbook_base[0])
 workbook_t19_url = r'{}{}'.format(PL_input_path,workbook_t19[0])
 workbook_t18_url = r'{}{}'.format(PL_input_path,workbook_t18[0])
 workbook_t17_url = r'{}{}'.format(PL_input_path,workbook_t17[0])
 workbook_t20_url = r'{}{}'.format(PL_input_path,workbook_t20[0])
-print("Workbook URLs are " + workbook_t19_url + ", " + workbook_t18_url + ", " + workbook_t17_url + ", " + workbook_t20_url)
+workbook_t21_url = r'{}{}'.format(PL_input_path,workbook_t21[0])
+workbook_t22_url = r'{}{}'.format(PL_input_path,workbook_t22[0])
+workbook_t23_url = r'{}{}'.format(PL_input_path,workbook_t23[0])
+workbook_t24_url = r'{}{}'.format(PL_input_path,workbook_t24[0])
+workbook_t25_url = r'{}{}'.format(PL_input_path,workbook_t25[0])
+workbook_t26_url = r'{}{}'.format(PL_input_path,workbook_t26[0])
+workbook_t27_url = r'{}{}'.format(PL_input_path,workbook_t27[0])
+print("Workbook URLs are " + workbook_t19_url + ", " + workbook_t18_url + ", " + workbook_t17_url
+      + ", " + workbook_t20_url + ", " + workbook_t21_url + ", " + workbook_t22_url + ", " + workbook_t23_url
+      + ", " + workbook_t24_url + ", " + workbook_t25_url + ", " + workbook_t26_url + ", " + workbook_t27_url)
 
 workbook_template = r'{}{}.xltx' .format(template_path,'Exe_Summary Table')
 print("Template URL is " + workbook_template)
@@ -33,6 +51,13 @@ t19_wb = openpyxl.load_workbook(workbook_t19_url)  # Add file name
 t18_wb = openpyxl.load_workbook(workbook_t18_url)  # Add file name
 t17_wb = openpyxl.load_workbook(workbook_t17_url)  # Add file name
 t20_wb = openpyxl.load_workbook(workbook_t20_url)  # Add file name
+t21_wb = openpyxl.load_workbook(workbook_t21_url)  # Add file name
+t22_wb = openpyxl.load_workbook(workbook_t22_url)  # Add file name
+t23_wb = openpyxl.load_workbook(workbook_t23_url)  # Add file name
+t24_wb = openpyxl.load_workbook(workbook_t24_url)  # Add file name
+t25_wb = openpyxl.load_workbook(workbook_t25_url)  # Add file name
+t26_wb = openpyxl.load_workbook(workbook_t26_url)  # Add file name
+t27_wb = openpyxl.load_workbook(workbook_t27_url)  # Add file name
 
 timeframes = ['Dept Cost by Category','Dept Cost by Mgt Group']
 selector_regex = re.compile(r'^(?!(TOC|Stock expense, 401K match and E|Bad Debt))')
@@ -268,6 +293,73 @@ bhntotal_sheet, bhntotal_max_row = createMergedSheet(target_worksheet, bhntotal_
                                        subtotalRows=False,
                                        totalColOffset=5, groupRows=False, grandTotal=False)
 ######   End of processing PL60_Dept Cost by Category by Seg
+######   Start of Processing PL61_Dept Cost By GAAP Line
+target_worksheet = target['Dept Cost by GAAP Line']
+
+selector_regex = re.compile(r'^(Technology|Ops, CC, Merch, Risk)')
+tps_group = 'Total Processing and Service - Dept. Costs'
+tpssheet, tps_max_row = createMergedSheet(target_worksheet, selector_regex, t21_wb, 1, 13, 9, 8, subtotalRows=True,
+                              totalColOffset=5, groupRows=True, grandTotal=True, grandTotalTitle=tps_group)
+selector_regex = re.compile(r'^(Cardpool & Product)')
+tcp_group = 'Total Costs of Products Sold - Dept. Costs'
+tcpsheet, tcp_max_row = createMergedSheet(target_worksheet, selector_regex, t21_wb, 1, tps_max_row+2, 9, 8, subtotalRows=True,
+                              totalColOffset=5, groupRows=True, grandTotal=True, grandTotalTitle=tcp_group)
+selector_regex = re.compile(r'^(Prod Mgt & Bus Dev|Marketing|US Sales|BES Sales|International Sales|International Sales - US)')
+tsm_group = 'Total Sales and Marketing - Dept. Costs'
+tsmsheet, tsm_max_row = createMergedSheet(target_worksheet, selector_regex, t21_wb, 1, tcp_max_row+2, 9, 8, subtotalRows=True,
+                              totalColOffset=5, groupRows=True, grandTotal=True, grandTotalTitle=tsm_group)
+selector_regex = re.compile(r'^(Legal|Acctg & Finance|HR|It Admin|Executive|Other|Bad Debt)')
+tga_group = 'Total General and Admin - Dept. Costs'
+tgasheet, tga_max_row = createMergedSheet(target_worksheet, selector_regex, t21_wb, 1, tsm_max_row+2, 9, 8, subtotalRows=True,
+                              totalColOffset=5, groupRows=True, grandTotal=True, grandTotalTitle=tga_group)
+
+target_worksheet.cell(row=tga_max_row+2, column=1).value = "TOTAL BLACKHAWK"
+for j in range(6, target_worksheet.max_column, 1):
+    target_cell = target_worksheet.cell(row=tga_max_row+2, column=j)
+    source = target_worksheet.cell(row=tga_max_row - 1, column=j)
+    if source.data_type == 'f':
+        target_cell.value = Translator(source.value, source.coordinate).translate_formula(target_cell.coordinate)
+    else:
+        target_cell.value = "=SUM({}+{}+{}+{})".format(target_worksheet.cell(row=tps_max_row, column=j).coordinate,
+                                                    target_worksheet.cell(row=tcp_max_row, column=j).coordinate,
+                                                    target_worksheet.cell(row=tsm_max_row, column=j).coordinate,
+                                                    target_worksheet.cell(row=tga_max_row, column=j).coordinate)
+    copy_style(source, target_cell)
+
+target_worksheet['A4'].value = tpssheet['A3'].value
+target_worksheet['A5'].value = tpssheet['J7'].value
+######   End of Processing PL61_Dept Cost By GAAP Line
+######   Start of Processing FTE, EMP & CNTR HC
+ftetimeframes = ['FTE ACT vs FCST','FTE CY vs PY','EMP ACT vs FCST','EMP CY vs PY','CNTR ACT vs FCST','CNTR CY vs PY']
+fteselector_regex = re.compile(r'^(?!(TOC))')
+
+for ftetimeframe in ftetimeframes:
+    target_worksheet = target[ftetimeframe]
+    first = True
+    if ftetimeframe == 'FTE ACT vs FCST':
+        source_wb = t22_wb
+        grouptitle = 'Total FTE HC'
+    if ftetimeframe == 'FTE CY vs PY':
+        source_wb = t23_wb
+        grouptitle = 'Total FTE HC'
+    if ftetimeframe == 'EMP ACT vs FCST':
+        source_wb = t24_wb
+        grouptitle = 'Total Employee HC'
+    if ftetimeframe == 'EMP CY vs PY':
+        source_wb = t25_wb
+        grouptitle = 'Total Employee HC'
+    if ftetimeframe == 'CNTR ACT vs FCST':
+        source_wb = t26_wb
+        grouptitle = 'Total Contractor HC'
+    if ftetimeframe == 'CNTR CY vs PY':
+        source_wb = t27_wb
+        grouptitle = 'Total Contractor HC'
+    basesheet, max_row = createMergedSheet(target_worksheet, fteselector_regex, source_wb, 1, 10, 9, 8, subtotalRows=True,
+                              totalColOffset=5, groupRows=True, grandTotal=True, grandTotalTitle=grouptitle)
+    target_worksheet['A3'].value = basesheet['A3'].value
+    target_worksheet['A4'].value = basesheet['A4'].value
+    target_worksheet['A5'].value = basesheet['T7'].value
+######   End of Processing FTE, EMP & CNTR HC
 target.save(result_workbook)
 
 wb1 = xw.Book(workbook_url)
